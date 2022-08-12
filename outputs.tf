6cf6b8186b16b81a0b2ef3e6e725da333ca1aa3e
Iain Black <iain@rootisgod.com>
2022-08-12 08:21:23 +0100

    Module to create a Resource Group

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+# Output all vars for later consumption
+
+output "resource_group_info" {
+  value = azurerm_resource_group.this
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "location" {
+  description   = "The location to place the RG"
+}
+
+variable "resource_group_name" {
+  description   = "The name of the Resource Group"
+}
